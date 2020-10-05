11bc41a014d4b41fe4f3ed6165b7fe8246644412
laconic23donetsk <laconic23donetsk@gmail.com>
2020-10-05 23:56:19 +0300

    Create pdf

--- TestCoreConsoleApp/Program.cs
+++ TestCoreConsoleApp/Program.cs
@@ -9,9 +9,24 @@ namespace TestCoreConsoleApp
     {
         static async Task Main(string[] args)
         {
+            using (var fs = new System.IO.FileStream(@"C:\Users\AndUser\Personal\tmp.pdf", System.IO.FileMode.Create, System.IO.FileAccess.Write))
+            {
+                byte[] byteArray = CreatePdf();
+                fs.Write(byteArray, 0, byteArray.Length);
+            }
+        }
 
+        public static byte[] CreatePdf()
+        {
+            var stream = new System.IO.MemoryStream();
+            var writer = new iText.Kernel.Pdf.PdfWriter(stream);
+            var pdf = new iText.Kernel.Pdf.PdfDocument(writer);
+            var document = new iText.Layout.Document(pdf);
 
-            Console.ReadKey();
+            document.Add(new iText.Layout.Element.Paragraph("Shit, man!"));
+            document.Close();
+
+            return stream.ToArray();
         }
     }
 }
