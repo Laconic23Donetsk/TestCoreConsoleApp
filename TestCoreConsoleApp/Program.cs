7c58cb822ca8f0a7b465d71144e0cc594048bc1f
laconic23donetsk <laconic23donetsk@gmail.com>
2021-07-28 08:49:22 +0300

    Generic classes with the same name

--- TestCoreConsoleApp/Program.cs
+++ TestCoreConsoleApp/Program.cs
@@ -11,7 +11,38 @@ namespace TestCoreConsoleApp
     {
         static async Task Main(string[] args)
         {
+            var class1 = new Class1<int>(3);
+            var class2 = new Class1<int, int>(1, 2);
 
+            class1.Foo();
+            class2.Foo();
+
+        }
+    }
+
+    class Class1<T> 
+    {
+        public Class1(T arg1)
+        {
+
+        }
+
+        public void Foo()
+        {
+            Console.WriteLine("version 1");
+        }
+    }
+
+    class Class1<T1, T2>
+    {
+        public Class1(T1 arg1, T2 arg2)
+        {
+
+        }
+
+        public void Foo()
+        {
+            Console.WriteLine("version 2");
         }
     }
 }
