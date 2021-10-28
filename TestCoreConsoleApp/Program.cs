05b6217d161e24bde0cc9834afe4a4910171f9fa
laconic23donetsk <laconic23donetsk@gmail.com>
2021-10-28 11:52:34 +0300

    Type.GetType -- fully qualified name

--- TestCoreConsoleApp/Program.cs
+++ TestCoreConsoleApp/Program.cs
@@ -11,7 +11,12 @@ namespace TestCoreConsoleApp
     {
         static async Task Main(string[] args)
         {
+            Type type = typeof(Newtonsoft.Json.IJsonLineInfo);
+            Console.WriteLine(type);
 
+
+            Type type2 = Type.GetType("Newtonsoft.Json.IJsonLineInfo, newtonsoft.json");
+            Console.WriteLine(type2);
         }
     }
 }
