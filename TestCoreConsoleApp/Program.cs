9b83658b9b625dcbc9f46b7b80dea0a1425b3032
laconic23donetsk <laconic23donetsk@gmail.com>
2021-10-28 11:46:31 +0300

    Deserialise JSON file

--- TestCoreConsoleApp/Program.cs
+++ TestCoreConsoleApp/Program.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Text.Json;
 
 namespace TestCoreConsoleApp
 {
@@ -11,7 +14,24 @@ namespace TestCoreConsoleApp
     {
         static async Task Main(string[] args)
         {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false);
+            IConfiguration config = builder.Build();
 
+            var section = config.GetSection("InfoLogExceptionFilter");
+            var infoLogExceptionFilterSettings = section.Get<InfoLogExceptionFilterSettings[]>();
+            //var settings = JsonSerializer.Deserialize<InfoLogExceptionFilterSettings>(json);
+
+            var jsonStr = JsonSerializer.Serialize(infoLogExceptionFilterSettings);
+            Console.WriteLine(jsonStr);
         }
+    }
+        
+    public class InfoLogExceptionFilterSettings
+    {
+        public string? Path { get; set; }
+        public string? Type { get; set; }
+        public string? Code { get; set; }
     }
 }
