35c5a18c1c016683036e4f138a815d41c5bbb03d
laconic23donetsk <laconic23donetsk@gmail.com>
2021-11-18 19:33:36 +0300

    Revert "Microsoft appsettings.json binder. Read into Dictionary"
    This reverts commit 3ca8b6ed53d616d35a0097104f7c4b0386574d0b.

--- TestCoreConsoleApp/Program.cs
+++ TestCoreConsoleApp/Program.cs
@@ -1,8 +1,5 @@
-﻿using Microsoft.Extensions.Configuration;
-using Newtonsoft.Json;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Threading;
@@ -14,57 +11,7 @@ namespace TestCoreConsoleApp
     {
         static async Task Main(string[] args)
         {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json", optional: false);
-            IConfiguration config = builder.Build();
 
-            var section = config.GetSection("RestServiceSettings");
-            var settings = section.Get<Dictionary<string, RestServiceSettings>>();
-
-            Console.WriteLine(settings["Pace Structure Service"]); 
         }
-    }
-
-
-    public record RestServiceSettings
-    {
-        public RestServiceSettings()
-        {
-        }
-
-        /// <summary>
-        /// Full Url to REST service endpoint
-        /// </summary>
-        public string? Url { get; init; }
-
-        /// <summary>
-        /// Full qualified interface name of REST service
-        /// </summary>
-        public string? ServiceContract { get; init; }
-
-        public HealthCheckSettings HealthCheckSettings { get; init; }
-
-    }
-
-    /// <summary>
-    /// Setting for health check configuration
-    /// </summary>
-    public record HealthCheckSettings
-    {
-        /// <summary>
-        /// Default URI for health check endpoint
-        /// </summary>
-        public const string DefaultHealthCheckUri = "/health";
-
-        /// <summary>
-        /// To check health of REST service
-        /// </summary>
-        public bool IsEnabled { get; set; } = true;
-
-        /// <summary>
-        /// Relative path to health check endpoint
-        /// </summary>
-        public string RelativePath { get; set; } = DefaultHealthCheckUri;
     }
 }
