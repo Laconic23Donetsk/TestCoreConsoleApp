f733a272e726f9ae5f6f1650dff3593eda863c9c
laconic23donetsk <laconic23donetsk@gmail.com>
2021-12-02 16:09:30 +0300

    Generic with T? (NOT WORKING)

--- TestCoreConsoleApp/Program.cs
+++ TestCoreConsoleApp/Program.cs
@@ -11,7 +11,36 @@ namespace TestCoreConsoleApp
     {
         static async Task Main(string[] args)
         {
+            Range<DateTime> range = new Range<DateTime>();
+            Validator validator = new Validator();
+            validator.IsValid(range);
+        }
+    }
 
+    public record Range<T>
+        where T : IComparable<T>
+    {
+        public Range(T? from, T? to)
+        {
+            From = from;
+            To = to;
+        }
+
+        public Range()
+        { }
+
+        public T? From { get; set; }
+        public T? To { get; set; }
+    }
+
+    public class Validator
+    {
+        public bool IsValid(Range<DateTime> dtRange)
+        {
+            if ((DateTime?)dtRange.From is null || (DateTime?)dtRange.To is null)
+                return true;
+
+            return dtRange.From > dtRange.To;
         }
     }
 }
