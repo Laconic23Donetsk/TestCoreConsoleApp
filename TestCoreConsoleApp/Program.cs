befe7a37944249ab35217010a6e39368ecdbda57
laconic23donetsk <laconic23donetsk@gmail.com>
2021-12-11 11:07:18 +0300

    read ctor parameter name via reflection

--- TestCoreConsoleApp/Program.cs
+++ TestCoreConsoleApp/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -10,6 +11,22 @@ namespace TestCoreConsoleApp
     class Program
     {
         static async Task Main(string[] args)
+        {
+            Type type = typeof(Test);
+            ConstructorInfo ctorInfo = type.GetConstructor(new[] { typeof(string) });
+            ParameterInfo[] parameters = ctorInfo.GetParameters();
+
+            foreach (ParameterInfo parameter in parameters)
+            {
+                if (parameter.ParameterType == typeof(string))
+                Console.WriteLine(parameter.Name);
+            }
+        }
+    }
+
+    public class Test
+    {
+        public Test(string paramName)
         {
 
         }
