8b38fb3078ffea7af1916b98f32b4baffe6174c7
laconic23donetsk <laconic23donetsk@gmail.com>
2021-12-11 11:07:27 +0300

    Revert "read ctor parameter name via reflection"
    This reverts commit befe7a37944249ab35217010a6e39368ecdbda57.

--- TestCoreConsoleApp/Program.cs
+++ TestCoreConsoleApp/Program.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
-using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -11,22 +10,6 @@ namespace TestCoreConsoleApp
     class Program
     {
         static async Task Main(string[] args)
-        {
-            Type type = typeof(Test);
-            ConstructorInfo ctorInfo = type.GetConstructor(new[] { typeof(string) });
-            ParameterInfo[] parameters = ctorInfo.GetParameters();
-
-            foreach (ParameterInfo parameter in parameters)
-            {
-                if (parameter.ParameterType == typeof(string))
-                Console.WriteLine(parameter.Name);
-            }
-        }
-    }
-
-    public class Test
-    {
-        public Test(string paramName)
         {
 
         }
