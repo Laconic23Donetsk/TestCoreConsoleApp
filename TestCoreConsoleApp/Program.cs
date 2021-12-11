f9d5f7a7a9dcf91fc7e21fada2eeaf4e40e47bc4
laconic23donetsk <laconic23donetsk@gmail.com>
2021-12-11 10:52:00 +0300

    Revert "work with ExceptionDispatchInfo"
    This reverts commit 14038adc4444b829ad46db368dcb1584a9eeaba3.

--- TestCoreConsoleApp/Program.cs
+++ TestCoreConsoleApp/Program.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
-using System.Runtime.ExceptionServices;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -12,41 +11,7 @@ namespace TestCoreConsoleApp
     {
         static async Task Main(string[] args)
         {
-            void Foo() => throw new InvalidOperationException("foo");
 
-            Exception original = null;
-            ExceptionDispatchInfo dispatchInfo = null;
-            try
-            {
-                try
-                {
-                    Foo();
-                }
-                catch (Exception ex)
-                {
-                    original = ex;
-                    dispatchInfo = ExceptionDispatchInfo.Capture(ex);
-                    throw ex;
-                }
-            }
-            catch (Exception ex2)
-            {
-                // ex2 is the same object as ex. But with a mutated StackTrace.
-                Console.WriteLine(ex2 == original);  // True
-            }
-
-            // So now "original" has lost the StackTrace containing "Foo":
-            Console.WriteLine(original.StackTrace.Contains("Foo"));  // False
-
-            // But dispatchInfo still has it:
-            try
-            {
-                dispatchInfo.Throw();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.StackTrace.Contains("Foo"));   // True
-            }
         }
     }
 }
